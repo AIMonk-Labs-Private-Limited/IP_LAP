04230b067d5a94f9d911f0c18e0b236f026bacd8
littleGiant-28 <vaibhavthakor2@gmail.com>
2024-01-03 14:04:50 +0530

    added multiprocessing for model inf, post proc and fac enh process

--- iplap_tritoninference.py
+++ iplap_tritoninference.py
@@ -4,6 +4,8 @@ import subprocess
 from tqdm import tqdm
 from models import Renderer
 import torch
+import torch.multiprocessing as multiprocessing
+multiprocessing.set_start_method('spawn')
 from models import Landmark_generator as Landmark_transformer
 import face_alignment
 from models import audio
@@ -11,6 +13,9 @@ from draw_landmark import draw_landmarks
 import mediapipe as mp
 from global_variable import *
 from inference_utils import *
+
+import time
+
 
 # from gfpgan import GFPGANer
 from pathlib import Path
@@ -26,12 +31,14 @@ import sys
 from bgremoval_package.demo.run import matting
 
 from bgremoval_package.demo.run import load_model as load_model_modenet
+
+MP_QUEUE_SIZE = 50
+
 class IPLAP_tritoninference:
     
     def __init__(self,landmark_gen_checkpoint_path,renderer_checkpoint_path):
         self.mp_face_mesh = mp.solutions.face_mesh
         self.drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=1, circle_radius=1)
-        self.fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, flip_input=False, device='cuda')
         # self.temp_dir = temp_dir
         
 
@@ -50,14 +57,50 @@ class IPLAP_tritoninference:
             model=Landmark_transformer(T=T, d_model=512, nlayers=4, nhead=4, dim_feedforward=1024, dropout=0.1),
             path=landmark_gen_checkpoint_path)
         self.renderer = load_model(model=Renderer(), path=renderer_checkpoint_path)
-        self.gfpgan=define_enhancer(method='gfpgan')
         self.modnet=load_model_modenet()
+        
+        # launch daemon processes for inf and post processing
+        self.launch_processes()
+        
+    def launch_processes(self):
+        self.input_queue = multiprocessing.Queue(maxsize=MP_QUEUE_SIZE)
+        self.inf_postproc_queue = multiprocessing.Queue(maxsize=MP_QUEUE_SIZE)
+        self.postproc_enh_queue = multiprocessing.Queue(maxsize=MP_QUEUE_SIZE)
+        self.output_queue =  multiprocessing.Queue(maxsize=MP_QUEUE_SIZE)
+        self.mp_lock = multiprocessing.Lock()
+        
+        # Shared float Value type objects to keep track of timings
+        self.model_inf_time = multiprocessing.Value('f', 0.0)
+        self.post_proc_time = multiprocessing.Value('f', 0.0)
+        self.face_enhc_time = multiprocessing.Value('f', 0.0)
+        
+        self.model_inf_process = multiprocessing.Process(
+            target=model_inference_process, 
+            args=(self.input_queue, self.inf_postproc_queue, self.landmark_generator_model,
+                 self.renderer, self.drawing_spec, self.model_inf_time),
+            daemon=True     # will get exited automatically once main process is completed/killed
+        )
+        
+        self.postproc_process = multiprocessing.Process(
+            target=postprocessing_process,
+            args=(self.inf_postproc_queue, self.postproc_enh_queue, self.mp_lock, self.post_proc_time),
+            daemon=True     # will get exited automatically once main process is completed/killed
+        )
+        
+        self.face_enhancer_process = multiprocessing.Process(
+            target=face_enhancer_process,
+            args=(self.postproc_enh_queue, self.output_queue, self.face_enhc_time),
+            daemon=True
+        )
+        
+        self.model_inf_process.start()
+        self.postproc_process.start()
+        self.face_enhancer_process.start()
     
     def infer(self,input_video_path,input_audio_path,temp_dir,bgremoval):
 
         # result_out_dir = os.path.dirname(temp_dir)
-        # import pdb;pdb.set_trace()
-
+        t0 = time.time()
         outfile_path = os.path.join(temp_dir,
                             '{}_N_{}_Nl_{}.mp4'.format(input_video_path.split('/')[-1][:-4] + 'result', ref_img_N, Nl))
 
@@ -81,20 +124,23 @@ class IPLAP_tritoninference:
         frame_h, frame_w, out_stream, input_mel_chunks_len, input_frame_sequence = prepare_output_stream(
             ori_background_frames, temp_dir, mel_chunks, input_vid_len, fps
         )
-        # import pdb;pdb.set_trace()
-        outfile_path =render_loop(
-            self.landmark_generator_model, self.renderer, self.drawing_spec, self.fa,temp_dir, input_mel_chunks_len, mel_chunks,
+
+        output_path, render_loop_time = global_render_loop(
+            self.input_queue, self.output_queue, input_mel_chunks_len, mel_chunks, 
             input_frame_sequence, face_crop_results, all_pose_landmarks, ori_background_frames,
-            frame_w, frame_h, ref_imgs, ref_img_sketches, out_stream, input_audio_path,
-            outfile_path, Nl_content, Nl_pose,self.gfpgan
+            frame_w, frame_h, ref_imgs, ref_img_sketches, Nl_content, Nl_pose, 
+            out_stream, input_audio_path, temp_dir, outfile_path
         )
-        # outfile_path =render_loop(
-        #     self.landmark_generator_model, self.renderer, self.drawing_spec, self.fa,temp_dir, input_mel_chunks_len, mel_chunks,
-        #     input_frame_sequence, face_crop_results, all_pose_landmarks, ori_background_frames,
-        #     frame_w, frame_h, ref_imgs, ref_img_sketches, out_stream, input_audio_path,
-        #     outfile_path, Nl_content, Nl_pose,self.gfpgan,
-        # )
-        # import pdb;pdb.set_trace()
+
+        timing_dict = {
+            'total_time': time.time() - t0,
+            'model_inf_time': self.model_inf_time.value,
+            'post_proc_time': self.post_proc_time.value,
+            'face_enhc_time': self.face_enhc_time.value,
+            'render_loop_time': render_loop_time
+        }
+        # print timing info
+        print("\n".join([f"{key}: {value}" for key, value in timing_dict.items()]))
         if bgremoval:
             output_path=os.path.join(temp_dir,"matts")
             os.makedirs(output_path,exist_ok=True)
